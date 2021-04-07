d14ba7efc7538bbb6f74333cbb62162758046589
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2021-04-07 15:04:45 +0200

    question service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../api/service/auth.service";
-import {FormBuilder} from "@angular/forms";
-import {Question} from "../../api/model/Question";
-import {QuestionService} from "../../api/service/question.service";
-import {AngularFirestore} from "@angular/fire/firestore";
-
 
 @Component({
   selector: 'app-home',
@@ -13,23 +7,11 @@ import {AngularFirestore} from "@angular/fire/firestore";
 })
 export class HomeComponent implements OnInit {
 
-  public question: Question = {detail: null} as Question;
-
   constructor(
-    public auth: AuthService,
-    public formBuilder: FormBuilder,
-    public firestore: AngularFirestore,
-    public questionService: QuestionService,
   ) {
   }
 
   ngOnInit(): void {
-  }
-
-
-  public send = (): void => {
-    if (!this.question.question) return;
-    this.questionService.sendQuestion(this.question);
   }
 
 }
