6f7d8d26649c4ce889a608bcd767f739d6a55923
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2021-04-11 22:14:17 +0200

    some responsivity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularFireModule} from "@angular/fire";
 import {environment} from "../environments/environment";
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {HomeComponent} from './components/home/home.component';
 import {FlipModule} from "ngx-flip";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
-import { HeaderComponent } from './components/header/header.component';
-import { QuestionComponent } from './components/question/question.component';
+import {HeaderComponent} from './components/header/header.component';
+import {QuestionComponent} from './components/question/question.component';
 import {TagInputModule} from "ngx-chips";
 import {MatRippleModule} from "@angular/material/core";
-import { ExploreComponent } from './components/explore/explore.component';
-import { VarDirective } from './api/directive/var.directive';
-import { QuestionsComponent } from './components/my-questions/questions.component';
-import { AnswersComponent } from './components/answers/answers.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import {ExploreComponent} from './components/explore/explore.component';
+import {VarDirective} from './api/directive/var.directive';
+import {QuestionsComponent} from './components/my-questions/questions.component';
+import {AnswersComponent} from './components/answers/answers.component';
+import {ProfileComponent} from './components/profile/profile.component';
 import {MatIconModule} from "@angular/material/icon";
-import { FooterComponent } from './components/footer/footer.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [
@@ -37,20 +38,22 @@ import { FooterComponent } from './components/footer/footer.component';
     ProfileComponent,
     FooterComponent
   ],
-    imports: [
-        AngularFireModule.initializeApp(environment.fireBaseConfig),
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        MatIconModule,
-        MatSnackBarModule,
-        TagInputModule,
-        ReactiveFormsModule,
-        FlipModule,
-        MatRippleModule,
-    ],
+  imports: [
+    AngularFireModule.initializeApp(environment.fireBaseConfig),
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    TagInputModule,
+    ReactiveFormsModule,
+    FlipModule,
+    MatRippleModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  fireBaseConfig: {
+    apiKey: "AIzaSyB1HNh6hIzdWjYTy99xCACdIxUhepdDfo8",
+    authDomain: "ask-away-f8640.firebaseapp.com",
+    projectId: "ask-away-f8640",
+    storageBucket: "ask-away-f8640.appspot.com",
+    messagingSenderId: "858871930596",
+    appId: "1:858871930596:web:485d18042946c4e025d6f1",
+    measurementId: "G-6LC8SV3CB2"
+  }
 };
